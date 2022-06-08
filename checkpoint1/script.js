8adb596a99a6f4f06892d20b3ef73cd408ba3df0
RiosBlack <glinhas55@hotmail.com>
2022-06-08 20:50:06 -0300

    adicionando responsividade

--- checkpoint1/script.js
+++ checkpoint1/script.js
@@ -1,22 +1,26 @@
 const form = document.querySelector('form');
-const tituloValue = document.getElementById('titleField')
-const descricaoValue = document.getElementById('descriptionField')
-const imageValue = document.getElementById('imageLink')
+const tituloValue = document.getElementById('titleField');
+const descricaoValue = document.getElementById('descriptionField');
+const imageValue = document.getElementById('imageLink');
 
-form.addEventListener('submit', (event) =>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    criarCard()
-})
+    criarCard();
+});
 
 function criarCard() {
-    let titulo = tituloValue.value
-    let descricao = descricaoValue.value
-    let imagem = imageValue.value
-    let cardCriado = `<div class="card">
-    <img src=${imagem} />
-    <h2>${titulo}</h2>
-    <p>${descricao}</p>
-    </div>`
+    let titulo = tituloValue.value;
+    let descricao = descricaoValue.value;
+    let imagem = imageValue.value;
+    let cardCriado = `<div class="bg-azul-claro p-1 flex flex-col rounded-lg justify-center w-3/4 lg:w-3/12 h-2/4">
+                        <img class="rounded-lg p-1" src=${imagem} />
+                        <div class="flex justify-center items-center text-black text-2xl">
+                            <h2>${titulo}</h2>
+                        </div>
+                        <div class="flex flex-wrap overflow-y-scroll overflow-visible">    
+                            <p class="break-normal overflow-wrap:normal">${descricao}</p>
+                        </div>
+                        </div>`;
 
-    document.querySelector('#cardSection').innerHTML+=cardCriado
+    document.querySelector('#cardSection').innerHTML += cardCriado;
 }
